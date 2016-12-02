9c7c9eeb80b8320ab7f3834357b44a553e405828
Haiyan Meng <hmeng@nd.edu>
2016-12-01 19:06:27 -0500

    Fix pipe.c

--- pipe.c
+++ pipe.c
@@ -29,7 +29,7 @@ int main(int argc, char *argv[]) {
 	int in[2] = {-1, -1};
 	int out[2] = {-1, -1};
 	int err[2] = {-1, -1};
-
+	
 	if(argc != 2) {
 		fprintf(stderr, "there should have two arguments!\n");
 		exit(EXIT_FAILURE);
@@ -37,6 +37,10 @@ int main(int argc, char *argv[]) {
 
 	input = argv[1];	
 	len = (size_t)snprintf(NULL, 0, "%s", input);
+
+	setbuf(stdin, NULL);
+	setbuf(stdout, NULL);
+	setbuf(stderr, NULL);
 
 	if(pipe(in) == -1) {
 		fprintf(stderr, "`pipe(in)` failed: %s\n", strerror(errno));
@@ -63,28 +67,50 @@ int main(int argc, char *argv[]) {
 		fprintf(stdout, "child process pipe out read end %d, write end: %d\n", out[0], out[1]);
 		fprintf(stdout, "child process pipe err read end %d, write end: %d\n", err[0], err[1]);
 
-		close(in[1]);
-		close(out[0]);
-		close(err[0]);
+		if(close(in[1])) {
+			fprintf(stderr, "`close(in[1])` failed: %s\n", strerror(errno));
+			exit(EXIT_FAILURE);
+		}
 
-		if(dup2(in[0], STDIN_FILENO)) {
+		if(close(out[0])) {
+			fprintf(stderr, "`close(out[0])` failed: %s\n", strerror(errno));
+			exit(EXIT_FAILURE);
+		}
+
+		if(close(err[0])) {
+			fprintf(stderr, "`close(err[0])` failed: %s\n", strerror(errno));
+			exit(EXIT_FAILURE);
+		}
+
+		if(dup2(in[0], STDIN_FILENO) == -1) {
 			fprintf(stderr, "`dup2(in[0], STDIN_FILENO)` failed: %s\n", strerror(errno));
 			exit(EXIT_FAILURE);
 		}
-		if(dup2(out[1], STDIN_FILENO)) {
-			fprintf(stderr, "`dup2(out[1], STDIN_FILENO)` failed: %s\n", strerror(errno));
+		if(dup2(out[1], STDOUT_FILENO) == -1) {
+			fprintf(stderr, "`dup2(out[1], STDOUT_FILENO)` failed: %s\n", strerror(errno));
 			exit(EXIT_FAILURE);
 		}
-		if(dup2(err[1], STDIN_FILENO)) {
-			fprintf(stderr, "`dup2(err[1], STDIN_FILENO)` failed: %s\n", strerror(errno));
+		if(dup2(err[1], STDERR_FILENO) == -1) {
+			fprintf(stderr, "`dup2(err[1], STDERR_FILENO)` failed: %s\n", strerror(errno));
 			exit(EXIT_FAILURE);
 		}
 
-		close(in[0]);
-		close(out[1]);
-		close(err[1]);
+		if(close(in[0])) {
+			fprintf(stderr, "`close(in[0])` failed: %s\n", strerror(errno));
+			exit(EXIT_FAILURE);
+		}
 
-		execlp("sh", "sh", "-c", "wc /home/hmeng/1", NULL);
+		if(close(out[1])) {
+			fprintf(stderr, "`close(out[1])` failed: %s\n", strerror(errno));
+			exit(EXIT_FAILURE);
+		}
+
+		if(close(err[1])) {
+			fprintf(stderr, "`close(err[1])` failed: %s\n", strerror(errno));
+			exit(EXIT_FAILURE);
+		}
+
+		execlp("sh", "sh", "-c", "wc", (char *)NULL);
 		fprintf(stderr, "child process `wc`, failed: %s\n", strerror(errno));
 		exit(EXIT_FAILURE);
 	}
@@ -94,30 +120,47 @@ int main(int argc, char *argv[]) {
 	fprintf(stdout, "parent process pipe out read end %d, write end: %d\n", out[0], out[1]);
 	fprintf(stdout, "parent process pipe err read end %d, write end: %d\n", err[0], err[1]);
 
-	close(in[0]);
-	close(out[1]);
-	close(err[1]);
-	
+	if(close(in[0])) {
+		fprintf(stderr, "`close(in[0])` failed: %s\n", strerror(errno));
+		exit(EXIT_FAILURE);
+	}
+
+	if(close(out[1])) {
+		fprintf(stderr, "`close(out[1])` failed: %s\n", strerror(errno));
+		exit(EXIT_FAILURE);
+	}
+
+	if(close(err[1])) {
+		fprintf(stderr, "`close(err[1])` failed: %s\n", strerror(errno));
+		exit(EXIT_FAILURE);
+	}
+
 	if(set_nonblock(in[1])) exit(EXIT_FAILURE);
 	if(set_nonblock(out[0])) exit(EXIT_FAILURE);
-	if(set_nonblock(out[0])) exit(EXIT_FAILURE);
+	if(set_nonblock(err[0])) exit(EXIT_FAILURE);
 
 	while(1) {
+		char output[100], error_data[100];
+		size_t len_out = 100, len_err = 100;
 		pid_t pid;
-		int *status;
+		int status;
 		ssize_t r;
-		int output[1048576], error[1048576];
-		size_t len_out, len_err;
 
-		if(len == 0 && in[1] > 0) close(in[1]);
+		if(len == 0 && in[1] > 0) {
+			if(close(in[1])) {
+				fprintf(stderr, "parent close(in[1]) failed: %s\n", strerror(errno));
+				exit(EXIT_FAILURE);
+			}
 
-		pid = waitpid(child, status, WNOHANG);
+			in[1] = -1;
+		}
+
+		pid = waitpid(child, &status, WNOHANG);
+
 		if(pid == -1) {
 			fprintf(stderr, "`waitpid` failed: %s\n", strerror(errno));
 			exit(EXIT_FAILURE);
 		}
-
-		if(pid > 0) break;
 
 		if(len) {
 			r = write(in[1], input, len);
@@ -142,19 +185,23 @@ int main(int argc, char *argv[]) {
 				fprintf(stderr, "`read(out[0], ...)` failed: %s\n", strerror(errno));
 				exit(EXIT_FAILURE);
 			}
-		} else {
-			fprintf(stdout, "the output of child process:\n%s\n\n", output);
+		} else if(r > 0){
+			output[r] = '\0';
+			fprintf(stdout, "the output of child process (size %d):\n%s", (int)r, output);
 		}
 
-		r = read(err[0], error, len_err);
+		r = read(err[0], error_data, len_err);
 		if(r == -1) {
 			if(errno != EAGAIN && errno != EINTR) {
 				fprintf(stderr, "`read(err[0], ...)` failed: %s\n", strerror(errno));
 				exit(EXIT_FAILURE);
 			}
-		} else {
-			fprintf(stderr, "the error of child process:\n%s\n\n", error);
+		} else if(r > 0){
+			error_data[r] = '\0';
+			fprintf(stderr, "the error of child process (size %d):\n%s", (int)r, error_data);
 		}
+
+		if(pid == child) break;
 	}
 
 	if(in[1] >= 0) close(in[1]);
